b666388440e2055ab59884ae1aac7d89713a34ce
Devraj-jha <devrajjha100806@gmail.com>
2026-09-01 23:51:48 +0530

    asychronous operations

--- concepts/asyc_await/index.js
+++ concepts/asyc_await/index.js
@@ -1,5 +1,8 @@
 // synchronous 
 
+const { RedIntegerFormat } = require("three");
+const { AsyncCompress } = require("three/examples/jsm/libs/fflate.module.js");
+
 console.log("a")
 console.log("b")
 console.log("c")
@@ -34,6 +37,63 @@ const promise = new Promise((re,rej) =>{
     }, 2000);
 });
 
-promise.then((result) => {
+promise.then(result => {
     console.log(result);
+}).catch(rej => {
+    console.log("rej");
 })
+
+// promise is a built in consturtor provided by js
+
+// these are old mehtods 
+
+// asyc await 
+
+async function main(){
+    const result = await promise; 
+     console.log(result)
+}
+
+function getdata(){
+    return new Promise((res) => {
+        setTimeout(() => {
+            resolve("hello world");
+        }, 2200);
+    })
+}
+// this above function replicates an api 
+
+async function main(){
+    console.log('hi');
+    const data = await getdata();
+
+    console.log(data);
+    console.log("error");
+}
+main();
+
+// error handling by try and catch 
+// .then and eatch
+
+
+async function getTasks() {
+
+    try {
+
+        const response = await fetch(
+            "http://localhost:3000/tasks"
+        );
+
+        const tasks = await response.json();
+
+        console.log(tasks);
+
+    } catch (error) {
+
+        console.log("Something went wrong:", error);
+
+    }
+}
+
+getTasks();
+
---
+++ concepts/constructor/index.js
@@ -0,0 +1,9 @@
+class person {
+    constructor (name , age){
+        this.name = name; 
+        this.age = age; 
+
+    }
+}
+let person1 = new person("devraj", 10); 
+

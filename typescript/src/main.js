cb76892befa9c0a3d51a74836f4075636c44b5d1
Devraj-jha <devrajjha100806@gmail.com>
2026-09-02 00:01:19 +0530

    ts

---
+++ typescript/src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const button = document.getElementById("btn");
+button?.addEventListener("click", () => {
+    console.log("Button clicked!");
+});
+//# sourceMappingURL=main.js.map
